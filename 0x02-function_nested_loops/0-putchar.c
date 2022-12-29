1a88f27bb33ac1171707e2e30748a2649664ea1e
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-28 20:08:04 -0800

    putchar

---
+++ 0x02-function_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -Prints _putchar, followed by a new line
+ * Return - Always 0 (success)
+ */
+int main(void)
+{
+	char a[] = "_putchar";
+	int b;
+
+	for (b = 0; b < 8; b++)
+	{
+		_putchar (a[b]);
+	}
+	_putchar('\n');
+	return (0);
+}
