4105ff9a97cb40dea5f47a36a5922fa87785929f
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-28 22:20:51 -0800

    alphabet_10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,7 +3,6 @@
 /**
  * print_alphabet_x10 - function that prints 10 times
  * the alphabet in lowercase
- * 
  * Return: Always 0 (success)
  */
 
@@ -11,9 +10,11 @@ void print_alphabet_x10(void)
 {
 	int b = 0;
 	while (b < 10)
+
 	{
 		char a = 'a';
 		while (a <= 'z')
+
 		{
 			_putchar(a);
 			a++;
