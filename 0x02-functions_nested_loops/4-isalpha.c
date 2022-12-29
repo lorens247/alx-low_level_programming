faf2874f6ed756912cc70bcba49aa38d005e7d31
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-29 11:11:46 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,22 +7,16 @@
  *
  * Return: Always 0.
  */
- 
  int _isalpha(int c)
  {
-     
-     if (c >= 97 && c <= 122)
-     {
-  
-	     return (1);
-     }
-
-     else if (c >= 65 && c <= 90)
-
-     {
-	     return (1);
-    }
-
-    return (0);
+	 if (c >= 97 && c <= 122)
+	 {
+		 return (1);
+	 }
+	 else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	 return (0);
 
 }
