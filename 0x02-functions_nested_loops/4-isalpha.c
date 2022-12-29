9e281c5662c87174fa3a4549a3a274f7e8eabece
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-29 11:05:34 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -13,17 +13,16 @@
      
      if (c >= 97 && c <= 122)
      {
-     
+  
 	     return (1);
      }
-     
-     else if (c >= 65 && c <= 90)
-     
-     {
- 
-     	     return (1);
-     }
- 
-     return (0);
 
- }
+     else if (c >= 65 && c <= 90)
+
+     {
+	     return (1);
+    }
+
+    return (0);
+
+}
