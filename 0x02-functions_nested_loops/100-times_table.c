d8790706b939c1cdeca0c2db5122b8096fbf9d0b
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-30 07:20:24 -0800

    100 times tables

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -29,7 +29,7 @@ void print_times_table(int n)
 				else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
-					_purchar(' ');
+					_putchar(' ');
 					_putchar(' ');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
