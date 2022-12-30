4900f52f1ca22b64c512b6c1adf001cf183a15b0
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-30 09:33:55 -0800

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	int count;
 	unsigned long f1 = 0, f2 = 1, sum;
 
-	for  (count = 0; count < 50; count++)
+	for (count = 0; count < 50; count++)
 	{
 		sum = f1 + f2;
 		printf("%lu", sum);
