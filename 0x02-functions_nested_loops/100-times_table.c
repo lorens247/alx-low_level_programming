1fa647c675448040af1ead9350ad2c033d06fd2c
Lawrence Ikhidero <lawrence@ikhidero.com>
2022-12-30 07:51:59 -0800

    100 times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,37 +11,30 @@ void print_times_table(int n)
 	{
 		for (i = 0; i <= n; i++)
 		{
+			_putchar('0');
+
 			for (j = 0; j <= n; j++)
 			{
+				_putchar(',');
+				_putchar(' ');
+				
 				k = j * i;
-				if (j == 0)
+
+				if (k <= 99)
+					_putchar(' ');
+				if (k <= 9)
+					_putchar(' ');
+
+				if (k >= 100)
 				{
-					_putchar(k + '0');
+					_putchar((j / 100) +'0');
+					_putchar (((j / 10)) % 10 + '0');
 				}
-				else if (k < 10 && j != 0)
+				else if (j <= 99 && j >= 10)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
+					_putchar((j / 10) + '0');
 				}
-				else if (k >= 10 && k < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				}
-				else if (k >= 100)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
-				}
+				_putchar((j % 10) + '0');
 			}
 			_putchar('\n');
 		}
