7bc271bfa0abb409579395a844f4d697f51e59e2
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-02 02:09:12 -0800

    print square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function that draws a square.
+ * @n : The number of lines using '\' characters to use
+ * Return: Void.
+ */
+void print_square(int n)
+{
+	int x, y;
+
+	for (y = 0; y < n; y++)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n')
+	}
+}
