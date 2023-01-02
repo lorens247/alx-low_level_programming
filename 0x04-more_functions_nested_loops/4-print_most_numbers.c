d205a29378064c435571561a3709ead7b995818f
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-01 22:19:43 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,14 @@
  * from 0 to 9 excluding 2 and 4
  * Return: Void.
  */
+
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != 2 || i != 4)
 		{
 			_putchar(i);
 		}
