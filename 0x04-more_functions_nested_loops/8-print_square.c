2ef7bab0929a7d52cfa9293819cd3b152e0e1e17
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-02 02:18:11 -0800

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -8,13 +8,14 @@
  */
 void print_square(int n)
 {
-	int x, y;
+	int x;
+	int y;
 
 	for (y = 0; y < n; y++)
 	{
 		for (x = 0; x < n; x++)
 		{
-			_putchar('#');
+			_putchar('n');
 		}
 		_putchar('\n');
 	}
