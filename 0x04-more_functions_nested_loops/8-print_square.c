b385aaa348fc3c0d2c24a13031e85bda2f545e50
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-02 11:28:20 -0800

    print square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,25 +3,25 @@
 
 /**
  * print_square - Function that draws a square.
- * @n : The number of lines using '\' characters to use
+ * @size : The number of lines using '\' characters to use
  * Return: Void.
  */
 
-void print_square(int n)
+void print_square(int size)
 {
-	int x;
-	int y;
+	int x, y;
 
-	for (y = 0; y < n; y++)
-	{
-		for (x = 0; x < n; x++)
-		{
-			_putchar('n');
-		}
+	if (size <= 0)
 		_putchar('\n');
-	}
-	if (n <= 0)
+	else
 	{
-		_putchar('\n')
+		for (x = 0; x < size; x++)
+		{
+			for (y = 0; y < size; y++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
 	}
 }
