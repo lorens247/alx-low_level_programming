83c93741302a85435594cd24c1c651fb42791934
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-01 21:54:17 -0800

    print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,12 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	char i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i);
-		}
+	if (!(i == '2' || i == '4'))
+	_putchar(i);
 	}
 	_putchar('\n');
 }
