d4484ec52121a5e7d3c9879d18510d9ad9d5052f
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-18 10:45:14 -0800

    main opcodes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * int_index - function that searches for an integer
+ * int_index - searches for an integer
  * @array: array with data
  * @size: size of the array
  * @cmp: function pointer
@@ -8,17 +8,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
+int i;
 
-    if (size <= 0)
-    return (-1);
-    if (array == NULL || cmp == NULL)
-    return (-1);
+if (size <= 0)
+return (-1);
+if (array == NULL || cmp == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i]) == 1)
-        return (i);
-    }
-    return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]) == 1)
+return (i);
+}
+return (-1);
 }
