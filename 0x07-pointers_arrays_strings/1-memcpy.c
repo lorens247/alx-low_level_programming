7715ba60a9de46ee7027fa4d2791925f774d2f8c
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-01-22 10:31:23 -0800

    crack me password

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function fills the first n bytes of the 
+ * memory area pointed to by s with the constant byte b
+ * @dest: pointer to char params
+ * @src: pointer to char params
+ * @n: size
+ * Return: *dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
