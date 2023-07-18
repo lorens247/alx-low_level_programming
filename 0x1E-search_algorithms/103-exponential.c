e3472256fbb9c0819c621ffe3e4dc1b3aab4060d
lorens247 <Lawrence Ikhidero>
2023-07-18 23:38:07 +0100

    updated

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,76 +1,16 @@
 #include "search_algos.h"
 
 /**
- * exponential_search - searches for a value in a sorted array of integers
- * using the Exponential search algorithm
- *
- * @array: pointer to the first element of the array
- * @size: number of elements in the array
- * @value: value to search for
- *
- * Return: the index where value is located,
- *         or -1 if value is not present in the array or if array is NULL
- */
-#include "search_algos.h"
-
-int _binary_search(int *array, int low, int high, int value);
-
-/**
- * _binary_search - searches for a value in a sorted array
- *	binary search
- *
- * @array: pointer to the first element of the array to search.
- * @low: lower bound of the search range.
- * @high: upper bound of the search range.
- * @value: the value to search for.
- *
- * Return: the index of the value;
- *	or -1 indicating the value is not present
- */
-int _binary_search(int *array, int low, int high, int value)
-{
-	int i, mid;
-
-	while (low <= high)
-	{
-		mid = low + (high - low) / 2;
-		printf("Searching in array: ");
-		for (i = low; i <= high; i++)
-		{
-			if (i == high)
-				printf("%d\n", array[i]);
-			else
-				printf("%d, ", array[i]);
-		}
-
-		if (array[mid] == value)
-			return (mid);
-		else if (array[mid] < value)
-			low = mid + 1;
-		else
-			high = mid - 1;
-	}
-
-	return (-1);
-}
-
-/**
  * exponential_search - searches for a value in a sorted array
  *	exponential search algorithm:
  *
  * @array: pointer to the first element of the array to search
  * @size: no. of elements in the array
+ * @low: lower bound of the search range.
+ * @high: upper bound of the search range.
  * @value: the value to search for.
- *
  * Return: the index of the value
  *	or -1 indicating the value is not present.
- *
- * Description: performs exponential search on a sorted array.
- *	prints the value checked at each step
- *	and the new array (or subarray) being searched.
- *
- *	uses powers of 2 as exponential ranges to search the array.
- *	if appropriate range is found, performs a binary search init
  */
 int exponential_search(int *array, size_t size, int value)
 {
